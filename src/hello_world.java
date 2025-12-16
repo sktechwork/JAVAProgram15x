7593dcb213f88a65679138d3320ab769e23e03b7
sayali <sayali.techwork@gmail.com>
2025-12-16 19:06:41 +0530

    2nd Commit on 16 dec 2025

---
+++ src/hello_world.java
@@ -0,0 +1,11 @@
+public class hello_world {
+
+    public static void main(String[] args) {
+
+        System.out.println("HELLO WORLD");
+        System.out.println("I am learning JAVA");
+        System.out.println("Let's get started");
+        System.out.println("New Print");
+
+           }
+}
