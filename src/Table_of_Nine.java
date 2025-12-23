ddf865925d3610a63512163a934e1ec9fef0bd03
sayali <sayali.techwork@gmail.com>
2025-12-23 15:26:35 +0530

    6th commit Table on nine

---
+++ src/Table_of_Nine.java
@@ -0,0 +1,27 @@
+public class Table_of_Nine {
+
+    static void main(String[] args) {
+
+
+        int a = 9;
+        System.out.printf("%d*1=%d", a, a*1);
+        System.out.println();
+        System.out.printf("%d*2=%d", a, a*2);
+        System.out.println();
+        System.out.printf("%d*3=%d", a, a*3);
+        System.out.println();
+        System.out.printf("%d*4=%d", a, a*4);
+        System.out.println();
+        System.out.printf("%d*5=%d", a, a*5);
+        System.out.println();
+        System.out.printf("%d*6=%d", a, a*6);
+        System.out.println();
+        System.out.printf("%d*7=%d", a, a*7);
+        System.out.println();
+        System.out.printf("%d*8=%d", a, a*8);
+        System.out.println();
+        System.out.printf("%d*9=%d", a, a*9);
+        System.out.println();
+        System.out.printf("%d*10=%d", a, a*10);
+    }
+}
