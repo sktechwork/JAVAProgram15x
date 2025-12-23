d34eb537776b8ad6c857e4e26ac29a3d9ce54dc8
sayali <sayali.techwork@gmail.com>
2025-12-23 14:22:10 +0530

    3rd commit

---
+++ src/Addition.java
@@ -0,0 +1,12 @@
+public class Addition {
+
+    static void main(String[] args) {
+
+        int a = 10;
+        int b = 49;
+
+        int c = a+b;
+
+        System.out.println("Addition of two numbers : " + c);
+    }
+}
