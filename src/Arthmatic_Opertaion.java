d803ce4ab31158aee8eebac201bb95938eb4b874
sayali <sayali.techwork@gmail.com>
2025-12-23 14:33:10 +0530

    4th

---
+++ src/Arthmatic_Opertaion.java
@@ -0,0 +1,22 @@
+public class Arthmatic_Opertaion {
+
+
+
+        static void main(String[] args) {
+
+            int x = 30;
+            int y = 10;
+            int z = 4;
+
+            int add = x+y+z;
+            int sub = x-y-z;
+            int mul = x*y*z;
+            int div = x/y/z;
+
+            System.out.println("Addition of 3 numbers :" +add);
+            System.out.println("Substraction of 3 numbers :" +sub);
+            System.out.println("Multiplication of 3 numbers :" +mul);
+            System.out.println("Division of 3 numbers :" +div);
+        }
+    }
+
