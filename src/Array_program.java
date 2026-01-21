b94d289935c40e6e2d8ac91a04b8014c8dc50fda
sayali <sayali.techwork@gmail.com>
2026-01-21 15:28:45 +0530

    JAVA String programs

---
+++ src/Array_program.java
@@ -0,0 +1,15 @@
+public class Array_program {
+    static void main(String[] args) {
+
+        int a1[] = {1, 2, 3, 4};
+        int a2[] = {3, 4, 5, 5};
+        System.out.println("Length: " +a1.length);
+        System.out.println(a1[0]);
+        System.out.println(a1[1]);
+        System.out.println(a1[2]);
+        System.out.println(a1[3]);
+        System.out.println(a1.equals(a2));
+
+
+    }
+}
