59d116ffcf7239c757c5d65a1741b7364e182cc3
sayali <sayali.techwork@gmail.com>
2026-01-22 12:02:43 +0530

    JAVA String programs

--- src/Array_program.java
+++ src/Array_program.java
@@ -1,14 +1,15 @@
+import java.util.Arrays;
+
 public class Array_program {
     static void main(String[] args) {
 
         int a1[] = {1, 2, 3, 4};
         int a2[] = {3, 4, 5, 5};
-        System.out.println("Length: " +a1.length);
-        System.out.println(a1[0]);
-        System.out.println(a1[1]);
-        System.out.println(a1[2]);
-        System.out.println(a1[3]);
+        System.out.println("Length of a1: " +a1.length);
+        System.out.println("Length of a2: " +a2.length);
         System.out.println(a1.equals(a2));
+        System.out.println(Arrays.toString(a1));
+        System.out.println(Arrays.toString(a2));
 
 
     }
--- src/ex_01_Java_basics/StingBuffer_function.java
+++ src/ex_01_Java_basics/StingBuffer_function.java
@@ -16,7 +16,23 @@ public class StingBuffer_function {
         System.out.println(sb.append("Kulkarni"));
         System.out.println(sb.charAt(2));
         System.out.println(sb.length());
-        System.out.println(sb.substring(1,3));
+        System.out.println(sb.substring(1,3)); //convert the string buffer to string
+        System.out.println(sb.toString());
+        System.out.println(sb.delete(1,3)); // delete the character 0,1,2 i.e. (n-1)
+        System.out.println(sb.indexOf("madam"));
+        System.out.println(sb.lastIndexOf("madam"));
+        System.out.println(sb.codePointAt(2)); // returns the ascii value
+
+        StringBuffer sb2 = new StringBuffer("MADAM");
+        System.out.println(sb.compareTo(sb2));
+
+        System.out.println(sb.equals(sb2)); //compare the value between two stringbuffer
+        System.out.println(sb.toString().repeat(2));
+
+        System.out.println(sb.isEmpty());
+        System.out.println(sb.capacity()); //returns the current capacity of the StringBuffer
+
+
 
 
     }
